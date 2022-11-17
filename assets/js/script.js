170fcba12ac8f74d777de7dbc450971885f52e76
Luis <Hairchevere@gmail.com>
2022-11-17 14:39:08 -0600

    working on get weather function

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,5 +8,25 @@ function startPage() {
     var currentWindEL = document.getElementById("wind");
     var currentUVEl = document.getElementById("UV");
     var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
-    
+
+    var APIKey = "a334fc8eab2946f09e9251d1c9294338"
+
+    function getWeather(cityName) {
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + APIKey;
+        fetch(queryURL)
+        .then(function(response){
+
+            var currentDate = new Date(response.formData.dt*1000);
+            var day = currentDate.getDate();
+            var month = currentDate.getMonth();
+            var year = currentDate.getFullYear();
+            nameEl.innerHTML = response.data.date + "(" + month + "/" + day + "/" + year + ")";
+            var weatherPic = response.data.weather[0].icon;
+            currentPicEl.setAttribute("src","https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
+        })
+
+        }
+
+    }
+
 }
