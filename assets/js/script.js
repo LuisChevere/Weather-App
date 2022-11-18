04e28ef2ac1b6264b97c68af1046fd715e21483f
Luis <Hairchevere@gmail.com>
2022-11-18 15:25:24 -0600

    added Search event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,54 +13,64 @@ function startPage() {
     function getWeather(cityName) {
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + APIKey;
         fetch(queryURL)
-        .then(function(response){
+            .then(function (response) {
 
-            var currentDate = new Date(response.formData.dt*1000);
-            var day = currentDate.getDate();
-            var month = currentDate.getMonth();
-            var year = currentDate.getFullYear();
-            nameEl.innerHTML = response.data.date + "(" + month + "/" + day + "/" + year + ")";
-            var weatherPic = response.data.weather[0].icon;
-            currentPicEl.setAttribute("src","https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
-            currentPicEl.setAttribute("alt", response.data.weather[0].description);
-            currentTempEl.innerHTML = "Temperature: " + k2f(response.data.main.temp) + "&#176F";
-            currentHumidityEl.innerHTML = "Humidity: " + response.data.main.humidity + "%";
-            currentWindEL.innerHTML = "Wind: " + response.data.wind.speed + " MPH";
+                var currentDate = new Date(response.formData.dt * 1000);
+                var day = currentDate.getDate();
+                var month = currentDate.getMonth();
+                var year = currentDate.getFullYear();
+                nameEl.innerHTML = response.data.date + "(" + month + "/" + day + "/" + year + ")";
+                var weatherPic = response.data.weather[0].icon;
+                currentPicEl.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
+                currentPicEl.setAttribute("alt", response.data.weather[0].description);
+                currentTempEl.innerHTML = "Temperature: " + k2f(response.data.main.temp) + "&#176F";
+                currentHumidityEl.innerHTML = "Humidity: " + response.data.main.humidity + "%";
+                currentWindEL.innerHTML = "Wind: " + response.data.wind.speed + " MPH";
 
-            var lat = response.data.coord.lat;
-            var lon = response.data.coord.lon;
-        });
-        
+                var lat = response.data.coord.lat;
+                var lon = response.data.coord.lon;
+            });
+
         var cityID = response.data.id;
         var forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID + "&appid=" + APIKey;
         fetch(forecastQueryURL)
-        .then(function(response){
-            var forecastEls = document.querySelectorAll(".forecast");
-            for (i=0; i < forecastEls.length; i++) {
-                forecastEls[i].innerHTML = "";
-                var forecastIndex =i*8 +4;
-                var forecastDate = new date(response.data.list[forecastIndex].dt * 1000);
-                var forecastDay = forecastDate.getDate();
-                var forecastMonth = forecastDate.getMonth() + 1;
-                var forecastYear = forecastDate.getFullYear();
-                var forecastDateEl = document.createElement("p");
-                forecastDateEl.setAttribute("class", "mt-3 mb-0 forecast-date");
-                forecastDateEl.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
-                forecastEls[i].append(forecastDateEl);
-                var forecastWeatherEl = document.createElement("img");
-                forecastWeatherEl.setAttribute("src","https://openweathermap.org/img/wn/" + response.data.list[forecastIndex].weather[0].icon + "@2x.png");
-                forecastWeatherEl.setAttribute("alt", response.data.list[forecastIndex].weather[0].description);
-                forecastEls[i].append(forecastWeatherEl);
-                var forecastTempEl = document.createElement("p");
-                forecastTempEl.innerHTML = "Temp: " + k2f(response.data.list[forecastIndex].main.temp) + "&#176F";
-                forecastEls[i].append(forecastTempEl);
-                var forecastHumidityEl = document.createElement("p");
-                forecastHumidityEl.innerHTML = "Humidity: " + response.data.list[forecastIndex].main.humidity + "%";
-                forecastEls[i].append(forecastHumidityEl);
-            }
+            .then(function (response) {
+                var forecastEls = document.querySelectorAll(".forecast");
+                for (i = 0; i < forecastEls.length; i++) {
+                    forecastEls[i].innerHTML = "";
+                    var forecastIndex = i * 8 + 4;
+                    var forecastDate = new date(response.data.list[forecastIndex].dt * 1000);
+                    var forecastDay = forecastDate.getDate();
+                    var forecastMonth = forecastDate.getMonth() + 1;
+                    var forecastYear = forecastDate.getFullYear();
+                    var forecastDateEl = document.createElement("p");
+                    forecastDateEl.setAttribute("class", "mt-3 mb-0 forecast-date");
+                    forecastDateEl.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
+                    forecastEls[i].append(forecastDateEl);
+                    var forecastWeatherEl = document.createElement("img");
+                    forecastWeatherEl.setAttribute("src", "https://openweathermap.org/img/wn/" + response.data.list[forecastIndex].weather[0].icon + "@2x.png");
+                    forecastWeatherEl.setAttribute("alt", response.data.list[forecastIndex].weather[0].description);
+                    forecastEls[i].append(forecastWeatherEl);
+                    var forecastTempEl = document.createElement("p");
+                    forecastTempEl.innerHTML = "Temp: " + k2f(response.data.list[forecastIndex].main.temp) + "&#176F";
+                    forecastEls[i].append(forecastTempEl);
+                    var forecastHumidityEl = document.createElement("p");
+                    forecastHumidityEl.innerHTML = "Humidity: " + response.data.list[forecastIndex].main.humidity + "%";
+                    forecastEls[i].append(forecastHumidityEl);
+                }
 
-        })
-
-        }
-
+            });
     }
+
+    searchEl.addEventListener("click",function(){
+        var searchTerm =inputEl.value;
+        getWeather(searchTerm);
+        searchHistory.push(searchTerm);
+        localStorage.setItem("search", JSON.stringify(searchHistory));
+        renderSearchHistory();
+    })
+
+    function k2f(K)
+    return Math.floor((K -273.15) *1.8 +32);
+}
+
