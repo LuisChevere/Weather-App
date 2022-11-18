615ca73693f6d4bb90e45e43debcf9be33724ff4
Luis <Hairchevere@gmail.com>
2022-11-18 15:31:00 -0600

    added search history function

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,3 +74,17 @@ function startPage() {
     return Math.floor((K -273.15) *1.8 +32);
 }
 
+function renderSearchHistory(){
+    historyEl.innerHTML = "";
+        for (var i=0; i < searchHistory.length; i++){
+            var historyItem =document.createElement("input");
+            historyItem.setAttribute("type","text");
+            historyItem.setAttribute("readonly",true);
+            historyItem.setAttribute("class", "form-control d-block bg-white");
+            historyItem.setAttribute("value", searchHistory[i]);
+            historyItem.addEventListener("click", function(){
+                getWeather(historyItem.value);
+            })
+            historyEl.append(historyItem);
+        }
+}
